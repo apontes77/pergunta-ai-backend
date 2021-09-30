af1a8eaa163d8744048a8c35e968ddefafae3e8d
apontes77 <alexandrepontesgyn@gmail.com>
2021-09-30 06:29:36 -0300

    tentativa de correção de uso de https no openapi

--- src/main/java/br/com/pucgo/perguntaai/PerguntaaiBackendApplication.java
+++ src/main/java/br/com/pucgo/perguntaai/PerguntaaiBackendApplication.java
@@ -1,28 +1,12 @@
 package br.com.pucgo.perguntaai;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PerguntaaiBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerguntaaiBackendApplication.class, args);
-	}
-
-	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI().addServersItem(new Server().url("https://pergunta-ai.azurewebsites.net/"))
-				.components(new Components().addSecuritySchemes("basicScheme",
-						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
-				.info(new Info().title("API Pergunta Aí").version("V1")
-						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 }
