35e63c12e0d99347e884672917d62db84a137015
apontes77 <alexandrepontesgyn@gmail.com>
2021-10-01 20:24:15 -0300

    test: libera cross origin a nível global

--- src/main/java/br/com/pucgo/perguntaai/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/pucgo/perguntaai/config/security/SecurityConfigurations.java
@@ -14,6 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -93,5 +98,15 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
 
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://pergunta-ai.azurewebsites.net/"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/br/com/pucgo/perguntaai/controllers/v1/UserController.java
+++ src/main/java/br/com/pucgo/perguntaai/controllers/v1/UserController.java
@@ -8,7 +8,10 @@ import br.com.pucgo.perguntaai.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
@@ -27,7 +30,6 @@ public class UserController {
 
     @PostMapping
     @Transactional
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @CacheEvict(value = "user", allEntries = true)
     public ResponseEntity<?> register(@RequestBody @Valid UserForm userForm,
                                       UriComponentsBuilder uriBuilder) {
