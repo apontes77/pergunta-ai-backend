3ba8cadb9ab77763cb73696a9c9584be60d471cf
apontes77 <alexandrepontesgyn@gmail.com>
2021-10-01 20:15:58 -0300

    test: libera cross origin

--- src/main/java/br/com/pucgo/perguntaai/controllers/v1/UserController.java
+++ src/main/java/br/com/pucgo/perguntaai/controllers/v1/UserController.java
@@ -8,10 +8,7 @@ import br.com.pucgo.perguntaai.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
@@ -30,6 +27,7 @@ public class UserController {
 
     @PostMapping
     @Transactional
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @CacheEvict(value = "user", allEntries = true)
     public ResponseEntity<?> register(@RequestBody @Valid UserForm userForm,
                                       UriComponentsBuilder uriBuilder) {
