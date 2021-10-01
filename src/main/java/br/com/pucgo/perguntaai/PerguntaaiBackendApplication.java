6046f57599ad6a8f66a64babb3447ae070c0a81d
apontes77 <alexandrepontesgyn@gmail.com>
2021-09-30 21:39:53 -0300

    adiciona permissao do CORS

--- src/main/java/br/com/pucgo/perguntaai/PerguntaaiBackendApplication.java
+++ src/main/java/br/com/pucgo/perguntaai/PerguntaaiBackendApplication.java
@@ -2,11 +2,25 @@ package br.com.pucgo.perguntaai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PerguntaaiBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerguntaaiBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api")
+						.allowedOrigins("https://pergunta-ai.azurewebsites.net/");
+			}
+		};
 	}
 }
