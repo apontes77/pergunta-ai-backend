aa9bf63f9eb7eb592974b2df9725b294f2984817
LaraCotrim <laracotrim@icloud.com>
2021-10-14 20:55:52 -0300

    PA-34 Mudança do retorno autenticação de usuário

--- src/main/java/br/com/pucgo/perguntaai/models/DTO/TokenDto.java
+++ src/main/java/br/com/pucgo/perguntaai/models/DTO/TokenDto.java
@@ -4,9 +4,11 @@ import lombok.Getter;
 
 @Getter
 public class TokenDto {
+    private Long id;
     private String token;
     private String type;
-    public TokenDto(String token, String type) {
+    public TokenDto(Long id, String token, String type) {
+        this.id = id;
         this.token = token;
         this.type = type;
     }
